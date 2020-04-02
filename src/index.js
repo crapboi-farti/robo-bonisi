c90de754d399bb6b104d84bf7aee6c89c2145f83
crapboi-farti <up.up.uppityy@gmail.com>
2020-04-02 01:43:01 +0100

    added a bunch of shit i lost track xd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { BONIS_PREFIX, BONIS_TOKEN, SERVER_ID, TEST_SERVER_ID, TEST_NEW_ROLE_ID, NEW_ROLE_ID, CHANNEL_PREFIX, TEST_CATEGORY_ID } = process.env;
+const { BONIS_PREFIX, BONIS_TOKEN, SERVER_ID, TEST_SERVER_ID, TEST_NEW_ROLE_ID, NEW_ROLE_ID, CHANNEL_PREFIX, TEST_CATEGORY_ID, TEST_HALA_MADANIYA } = process.env;
 const { CommandoClient } = require('discord.js-commando');
 const client = new CommandoClient({commandPrefix: BONIS_PREFIX});
 
@@ -11,18 +11,32 @@ client.on("ready", () => {
     console.log(`logged in as ${client.user.username}`);
 });
 
-client.once("guildMemberAdd", async member => {
-    let serverid = client.guilds.cache.get(TEST_SERVER_ID);
-    let role = serverid.roles.cache.get(TEST_NEW_ROLE_ID);
-    try {
-        await member.roles.add(role.id);
-    } catch (e) {
-        console.error(e);
-    }
-    serverid.channels.create(`${CHANNEL_PREFIX}${member.id}`, "text").then(channel => {
-        channel.setParent(TEST_CATEGORY_ID);
-        channel.send(`greetings Mr./Ms. ${member.user.username}, please provide us with your Facebook name below.`);
+client.on("guildMemberAdd", async member => {
+    let server = client.guilds.cache.get(TEST_SERVER_ID);
+    let role = server.roles.cache.get(TEST_NEW_ROLE_ID);
+    await member.roles.add(role.id);
+    server.channels.create(`${CHANNEL_PREFIX}${member.id}`).then(guest_channel => {
+        server.channels.cache.forEach(server_channels => {
+            if(server_channels.id === guest_channel.id) return;
+            server_channels.updateOverwrite(member.id, {VIEW_CHANNEL: false}).catch(console.error);
+        });
+        guest_channel.setParent(TEST_CATEGORY_ID);
+        guest_channel.send(`Greetings Mr./Ms. ${member.user.username}, please provide us with your Facebook name below.`);
+        client.on("message", message => {
+            if(message.channel.id !== guest_channel.id) return;
+            const last_message = guest_channel.lastMessage;
+            if(last_message.author.bot) return;
+            const hala_madanya = client.channels.cache.get(TEST_HALA_MADANIYA);
+            console.log(last_message.content);
+            hala_madanya.send(`${last_message.author}'s Facebook is: ${last_message.content}`);
+            server.channels.cache.forEach(guild_channels => {
+                guild_channels.updateOverwrite(member.id, {VIEW_CHANNEL: true}).catch(console.error);
+            })
+            guest_channel.delete();
+            member.roles.remove(role.id);
+        });
     });
+    
 });
 
 client.login(BONIS_TOKEN);
